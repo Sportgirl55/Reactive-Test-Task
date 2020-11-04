89d45e16925f5e82b8dd49e0cea19a97706b3700
Elena Slyusareva <sportgirl55@bk.ru>
2020-11-05 03:02:35 +0500

    Radio-group is almost ready

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const radioButton = Array.from(document.querySelectorAll('.radio'));
+
+
+
+
+
+// let $radioButton =  $('.radio-item');
+
+
+// $radioButton.parent().css('border', '3px solid red');
+
+// $radioButton.addEventListener('click', (e) => {
+//                e.currentTarget.style.border = '3px solid red';
+//           })
+
+//           $radioButton.onclick = function (e) {
+//                e.currentTarget.style.border = '3px solid red';
+//           }
+
+
+
+
+// .css('color', 'red');
+
+// for (let radio of radioButton) {
+//      radio.addEventListener('click', (e) => {
+//           e.currentTarget.style.border = '3px solid red';
+//      })
+// }
