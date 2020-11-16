75079050d4be6c510034b5b559042eba85f6221a
Elena Slyusareva <sportgirl55@bk.ru>
2020-11-16 12:20:37 +0500

    Final edition

--- js/script.js
+++ js/script.js
@@ -1,78 +1,63 @@
-'use strict';
+// Переключение радиокнопок
 
-//Переключение радиокнопок
 $('.radio-item').on('click', function() {
-  $(this).addClass('check').siblings().removeClass('check');
+    $(this).addClass('check').siblings().removeClass('check');
+  })
+  
+  //Переключение чекбоксов
+  
+  $('input:checkbox').click(function(){
+      if ($(this).is(':checked')) {
+          $(this).parent().addClass('check');
+      } else {
+          $(this).parent().removeClass('check');
+      }
+  });
+
+// Основная логика переключения секций
+
+const steps = [...document.querySelectorAll('.step')];
+const btsNext = [...document.querySelectorAll('.next')];
+const btsChange = [...document.querySelectorAll('.btn-change')];
+const btnSubmit = document.querySelector('.submit');
+const checkBox = [...document.querySelectorAll('input[type=checkbox]')];
+
+btnSubmit.setAttribute('disabled', true);
+steps[steps.length - 1].classList.add('hidden');
+
+checkBox.forEach((element) => {
+  element.addEventListener('change', () => {
+    if (checkBox.every((element) => element.checked)) {
+      btnSubmit.removeAttribute('disabled');
+    }
+  })
 })
 
-//Переключение чекбоксов
-
-$('input:checkbox').click(function(){
-	if ($(this).is(':checked')) {
-		$(this).parent().addClass('check');
-	} else {
-		$(this).parent().removeClass('check');
-	}
-});
-
-//Чекбоксы и кнопка "Продолжить"
-// Работает только для одного чекбокса, а нужно для двух
-
-$('input:checkbox').click(function(){
-	if ($(this).is(':checked')){
-		$('#agree').removeAttr('disabled');
-	} else {
-		$('#agree').attr('disabled', 'disabled'); 
-	}
-});
-
-//Кнопки "Продолжить"
-
-const buttons = Array.from(document.querySelectorAll('button'));
-const main = document.querySelector('#main');
-
-for (let btn of buttons) {
-  btn.addEventListener('click', (e) => {
-    if (e.currentTarget.closest('section') != main.lastElementChild) {
-      e.currentTarget.closest('.form-item').classList.add('hidden');
-      e.currentTarget.closest('.form-item').nextElementSibling.classList.remove('hidden');
-    let nextSibChildren = e.currentTarget.closest('.form-item').parentElement.nextElementSibling.children;
-      for (let child of nextSibChildren) {
-        if (child.hasAttribute('data-state')) {  
-          child.getAttribute('data-state') == "active" ? child.classList.remove('hidden') : null;
-          child.getAttribute('data-state') == "hidden" ? child.classList.add('hidden') : null;
-        }
+btsNext.forEach((element, i) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+      steps[i].classList.remove('open');
+      steps[i].classList.add('change');
+      steps[i + 1].classList.add('open');
+      steps[i + 1].classList.remove('hidden');
+      if (i === steps.length - 2) {
+        steps[i + 1].classList.add('done');
+        [...steps].splice(0, i + 1).forEach((element) => 
+        element.classList.add('hidden'),
+        );
       }
-    } else {
-     for (let item of document.querySelector('#main').children)
-      item != main.lastElementChild ? item.classList.add('hidden') : null;
-      main.lastElementChild.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
-      main.lastElementChild.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-
-    }
-  });
-}
-
-//Кнопки "Изменить"
-
-const changeBtn = Array.from(document.querySelectorAll('.change'));
-for (let change of changeBtn) {
-  change.addEventListener('click', (e) => {
-    document.querySelectorAll('.form-item[data-state="active"]').forEach(function (elem) {
-      elem.classList.add('hidden');
     })
-    document.querySelectorAll('.form-item[data-state="checked"]').forEach(function (elem) {
-      elem.classList.add('hidden');
-    })
-    document.querySelectorAll('.form-item[data-state="hidden"]').forEach(function (elem) {
-      elem.classList.remove('hidden');
-    })
+  }
+);
 
-    e.currentTarget.closest('.form-item').classList.add('hidden');
-    e.currentTarget.closest('section').querySelector('.form-item[data-state="active"]').classList.remove('hidden');
-    if (e.currentTarget.closest('section').previousElementSibling != null) {
-      e.currentTarget.closest('section').querySelector('.form-item[data-state="hidden"]').classList.add('hidden');
-      e.currentTarget.closest('section').previousElementSibling.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
-    } 
-  })
-}
+btsChange.forEach((element, i) => {
+    element.addEventListener('click', () => {
+      [...steps].splice(i).forEach((element) => {
+        element.classList.remove('change');
+        element.classList.remove('open');
+        element.classList.remove('done');
+      })
+      steps[i].classList.add('open');
+    })
+  }
+);
